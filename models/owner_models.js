82fff7158835a72ff2eebbdae25d25289a7b4b8d
aryanverma2601 <aryanv380@gmail.com>
2025-06-22 18:48:36 +0530

    Added separation of concerns and created models and routes

---
+++ models/owner_models.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const ownerSchema = mongoose.Schema({
+  fullname: {
+    type: String,
+    trim: true,
+    minLength: 3,
+  },
+  email: String,
+  password: Stirng,
+  isadmin: Boolean,
+  picture: String,
+  gstin: String,
+});
+
+module.exports = mongoose.model("owner", ownerSchema);
